fb668a60bed308337d194ef8cd105eb14701efcd
Brendson <brendson.net@gmail.com>
2023-02-02 11:23:49 -0300

    feat: criando model de usuário

--- Models/Usuario.cs
+++ Models/Usuario.cs
@@ -1,9 +1,20 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace usuario_net.Models
 {
     public class Usuario
     {
         public int Id { get; set; }
         public string Nome { get; set; }
+        public string Email { get; set; }
+        public string Senha { get; set; }
         public DateTime DataNascimento { get; set; }
+
+        public Usuario(string nome, string email, string senha, DateTime dataNascimento) {
+            Nome = nome;
+            Email = email;
+            Senha = senha;
+            DataNascimento = dataNascimento;
+        }
     }
 }

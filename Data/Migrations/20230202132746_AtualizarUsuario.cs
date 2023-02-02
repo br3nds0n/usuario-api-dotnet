0100fa8fad3de19f7177402864c667df69b0032b
Brendson <brendson.net@gmail.com>
2023-02-02 11:25:22 -0300

    feat: criando migrations

---
+++ Data/Migrations/20230202132746_AtualizarUsuario.cs
@@ -0,0 +1,145 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace usuario_net.Migrations
+{
+    public partial class AtualizarUsuario : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Usuarios",
+                table: "Usuarios");
+
+            migrationBuilder.RenameTable(
+                name: "Usuarios",
+                newName: "tb_usuario");
+
+            migrationBuilder.RenameColumn(
+                name: "Senha",
+                table: "tb_usuario",
+                newName: "senha");
+
+            migrationBuilder.RenameColumn(
+                name: "Nome",
+                table: "tb_usuario",
+                newName: "nome");
+
+            migrationBuilder.RenameColumn(
+                name: "Email",
+                table: "tb_usuario",
+                newName: "email");
+
+            migrationBuilder.RenameColumn(
+                name: "Id",
+                table: "tb_usuario",
+                newName: "id");
+
+            migrationBuilder.RenameColumn(
+                name: "DataNascimento",
+                table: "tb_usuario",
+                newName: "data_nascimento");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "senha",
+                table: "tb_usuario",
+                type: "text",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "text",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "nome",
+                table: "tb_usuario",
+                type: "text",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "text",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "email",
+                table: "tb_usuario",
+                type: "text",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "text",
+                oldNullable: true);
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_tb_usuario",
+                table: "tb_usuario",
+                column: "id");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_tb_usuario",
+                table: "tb_usuario");
+
+            migrationBuilder.RenameTable(
+                name: "tb_usuario",
+                newName: "Usuarios");
+
+            migrationBuilder.RenameColumn(
+                name: "senha",
+                table: "Usuarios",
+                newName: "Senha");
+
+            migrationBuilder.RenameColumn(
+                name: "nome",
+                table: "Usuarios",
+                newName: "Nome");
+
+            migrationBuilder.RenameColumn(
+                name: "email",
+                table: "Usuarios",
+                newName: "Email");
+
+            migrationBuilder.RenameColumn(
+                name: "id",
+                table: "Usuarios",
+                newName: "Id");
+
+            migrationBuilder.RenameColumn(
+                name: "data_nascimento",
+                table: "Usuarios",
+                newName: "DataNascimento");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Senha",
+                table: "Usuarios",
+                type: "text",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "text");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Nome",
+                table: "Usuarios",
+                type: "text",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "text");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Email",
+                table: "Usuarios",
+                type: "text",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "text");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Usuarios",
+                table: "Usuarios",
+                column: "Id");
+        }
+    }
+}
---
+++ Data/Migrations/20230202135117_AtualizarEmail.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace usuario_net.Migrations
+{
+    public partial class AtualizarEmail : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}

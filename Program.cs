85007579c95516e8e598df6a450c9c9c70bc06a1
Brendson <brendson.net@gmail.com>
2023-02-02 11:23:31 -0300

    feat: adicionando configuraçaõ do db

--- Program.cs
+++ Program.cs
@@ -1,13 +1,23 @@
+using Microsoft.EntityFrameworkCore;
+using usuario_net.Data.Context;
+using usuario_net.Data.Repository;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Services.AddDbContext<UsuarioContext>(options =>
+{
+    options.UseNpgsql(builder.Configuration.GetConnectionString("Default"));
+});
+builder.Services.AddScoped<IUsuarioRepository, UsuarioRepository>();
+
 var app = builder.Build();
 
 if (app.Environment.IsDevelopment())
-{   
+{
     app.UseSwagger();
     app.UseSwaggerUI();
 }
